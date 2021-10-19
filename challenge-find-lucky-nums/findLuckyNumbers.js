25470d9680b08f09d8cd27644e75e2548874de24
jerahmeelwilson <wilsonjerahmeel@gmail.com>
2021-10-19 09:37:41 -0500

    added solution for rlucky num

--- challenge-find-lucky-nums/findLuckyNumbers.js
+++ challenge-find-lucky-nums/findLuckyNumbers.js
@@ -1,1 +1,25 @@
 // Write your code below this line.
+
+function randomNum(){
+    return Math.floor(Math.random() * 10);
+}
+
+function luckyNumbers(n){
+    let luckyNums = [];
+
+    //generate random number 1 - n
+    //if number is not in luckyNums add to array.
+    //rerun n times
+    luckyNums.push(randomNum());
+    let count = 1;
+    while(count!=n){
+        let newRandomNum = randomNum();
+        if(luckyNums.includes(newRandomNum)===false){
+            luckyNums.push(newRandomNum)
+            count++;
+        }
+    }
+    return luckyNums;
+}
+
+console.log(luckyNumbers(10));
