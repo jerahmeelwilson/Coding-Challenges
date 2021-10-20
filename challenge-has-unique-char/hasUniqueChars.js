7862286d49c41c16a43886fc684c117efc5b20ab
jerahmeelwilson <wilsonjerahmeel@gmail.com>
2021-10-20 09:14:56 -0500

    Added solution for has unique char

--- challenge-has-unique-char/hasUniqueChars.js
+++ challenge-has-unique-char/hasUniqueChars.js
@@ -1,1 +1,22 @@
 // Write your code below
+
+function charMatch(char, word){
+    const regex = new RegExp(char,"g");
+    return word.match(regex).length
+}
+
+function hasUniqueChars(word){
+    for(let i = 0; i < word.length;i++){
+        if(charMatch(word[i],word) > 1){
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(hasUniqueChars("Monday"));
+console.log(hasUniqueChars("Moonday"));
+console.log(hasUniqueChars("Bob"));
+console.log(hasUniqueChars("Road"));
+console.log(hasUniqueChars("Roada"));
+console.log(hasUniqueChars("Jerahmeel"));
