b8367c380b6dd6b81d21760e4779af6a4f21950a
jerahmeelwilson <wilsonjerahmeel@gmail.com>
2021-10-22 10:49:53 -0500

    Added solutions

---
+++ chal-pig-latin/chal-pig-latin-solution/pigLatin.js
@@ -0,0 +1,17 @@
+// Write your solution below:
+
+function pigLatin(sentence) {
+  let words = sentence.split(" ")
+  for (let i = 0; i < words.length; i++) {
+    if (words[i][0] === "a" || words[i][0] === "e" || words[i][0] === "i" || words[i][0] === "o" || words[i][0] === "u") {
+      words[i] += "yay"
+    } else {
+      words[i] += words[i][0]
+      words[i] += "ay"
+      words[i] = words[i].substring(1)
+    }
+  }
+  console.log(words)
+}
+pigLatin("porcupines are cute");
+pigLatin("give me an apple");
--- chal-pig-latin/pigLatin.js
+++ chal-pig-latin/pigLatin.js
@@ -1,2 +1,18 @@
 // Write your solution below:
 
+function pigLatin(phrase){
+    let phraseArray = phrase.split(" ");
+    let newPhrase = "";
+    phraseArray.forEach(element => {
+        if(element[0].match(/[^aieou]/g) != null){
+            newPhrase += element.slice(1) + element[0] +"ay ";
+        } else{
+            newPhrase += element + "yay "
+        }
+    });
+    return newPhrase;
+}
+
+console.log(pigLatin("porcupines are cute"));
+console.log(pigLatin("give me an apple"));
+
