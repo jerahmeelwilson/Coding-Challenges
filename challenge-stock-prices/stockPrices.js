29644bb6eea022bfbf5d872ce30fe576404b47b9
jerahmeelwilson <wilsonjerahmeel@gmail.com>
2021-10-27 09:30:45 -0500

    Added solutions for string decoder

--- challenge-stock-prices/stockPrices.js
+++ challenge-stock-prices/stockPrices.js
@@ -18,6 +18,7 @@ const best = (stocks) => {
 
 
 console.log(best([15, 10, 20, 22, 1, 9]));
+
 console.log(best([2, 3, 10, 6, 4, 8, 1]));
 console.log(best([7, 9, 5, 6, 3, 2]));
 console.log(best([0, 100]));
---
+++ challenge-string-decoder/stringDecoder.js
@@ -0,0 +1,16 @@
+function decoder(code){
+    let word = "";
+    for(let i = 0; i<code.length;i++){
+       if(Number(code[i]) || code[i] == 0){
+         word += code[1+i+Number(code[i])];
+       }
+    }
+    return word;
+}
+
+console.log(decoder("0h2xce5ngbrdy"));
+console.log(decoder("3vdfn"));
+console.log(decoder("2bna0p1mp2osl0e"));
+console.log(decoder("0r"));
+console.log(decoder("0y4akjfe0s"));
+
