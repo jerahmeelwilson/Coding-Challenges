4c37b740633422c5f9919c6007da4d172bcca1fc
jerahmeelwilson <wilsonjerahmeel@gmail.com>
2021-11-09 09:27:53 -0600

    Added chal-phone-number

---
+++ chal-phone-number/phoneNumber.js
@@ -0,0 +1,12 @@
+//Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+const createPhoneNumber = (number) => {
+  number.unshift("(");
+  number.splice(4, 0, ") ");
+  number.splice(8, 0, "-");
+  return number.join("");
+};
+
+console.log(createPhoneNumber(number));
